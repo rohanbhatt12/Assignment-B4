e262764d74ce658ca2997618cda26ddeb5425e4c
Rohan Bhatt <rb954@scarletmail.rutgers.edu>
2020-04-12 19:08:47 -0400

    b4

---
+++ Assets/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public float speed = 7.0f;
+    private Transform target;
+
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update() {
+        transform.LookAt(target.position);
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+}
